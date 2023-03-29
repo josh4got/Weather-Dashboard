8dc6a8519bbfce2b59a5a4207855786491ee053c
josh4got <125082025+josh4got@users.noreply.github.com>
2023-03-29 17:42:06 -0600

    initial files added

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var today = $("#today");
+var fiveday = $("#5day");
+var search = $("#city");\
+var SBTN = $("#searchbtn")
+
+var history = [];
+console.log(SBTN)
+SBTN.on("click", function () {
+  console.log("clicked");
+});

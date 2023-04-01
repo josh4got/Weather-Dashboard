0f477ed2c1431bb7086264ab3796cf6228868912
josh4got <125082025+josh4got@users.noreply.github.com>
2023-04-01 17:02:31 -0600

    added function to append weather cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,107 @@
-var today = $("#today");
-var fiveday = $("#5day");
-var search = $("#city");
+let apiUrl = "https://api.openweathermap.org/data/2.5/forecast";
+let apiKey = "eceeec2fb91796f6d65128e7d90aad46";
+let history = [];
 
-var history = [];
-
+// event listener for the search button
 $("#form").on("submit", function (event) {
   event.preventDefault();
-
-  // call the storeText function to store the form text in local storage
+  getWeather($("#city").val());
+  if (jQuery.inArray($("#city").val(), history) !== -1) {
+    return;
+  }
+  history.push($("#city").val());
   storeText();
+});
+// event listener for the search history buttons
+$(".btn-secondary").on("click", function () {
+  preventDefault();
+  stoppropagation();
+  var formText = $(this).text();
+  getWeather(formText);
 });
 
 function storeText() {
-  const formText = $("#city").val();
+  let formText = $("#city").val();
   localStorage.setItem(formText, formText);
   console.log(formText);
-  $("#searched").append("<li>" + formText + "</li>");
+  $("#searched").append(
+    "<button class='btn btn-secondary col-12'>" + formText + "</button>"
+  );
 }
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
-const apiKey = "eceeec2fb91796f6d65128e7d90aad46";
-
-// define a function to get the weather for a city
+// function to call to the url
 function getWeather(city) {
-  // construct the API endpoint URL with the city name and API key
-  const url = `${apiUrl}?q=${city}&appid=${apiKey}`;
-
-  // use the fetch API to make a GET request to the API endpoint URL
+  let url = `${apiUrl}?q=${city}&units=imperial&appid=${apiKey}`;
+  // fetch from the open weather api
   fetch(url)
     .then((response) => {
-      // if the response is successful, parse the JSON data and log it to the console
       if (response.ok) {
         return response.json();
       }
-      // if there is an error, throw an error with the status code and status text
-      throw new Error(
-        `HTTP error! status: ${response.status} ${response.statusText}`
-      );
     })
     .then((data) => {
-      // log the parsed data to the console
-      console.log(data);
-    })
-    .catch((error) => {
-      // log any errors to the console
-      console.error(error);
+      $("#today").append(
+        "<div class = 'card2 mb-3'>" +
+          "<h4>" +
+          $("#city").val() +
+          "<h4/>" +
+          "<h5>" +
+          data.list[0].dt_txt.slice(0, 10) +
+          "<h5/>" +
+          "<p>" +
+          "Temp: " +
+          data.list[0].main.temp +
+          "°F" +
+          "<p/>" +
+          "<p>" +
+          "humidity: " +
+          data.list[0].main.humidity +
+          "%" +
+          "<p/>" +
+          "<p>" +
+          "Wind Speed: " +
+          data.list[0].wind.speed +
+          "mph" +
+          "<p/>" +
+          "</div>"
+      );
+      if ($(".card2").length >= 2) {
+        $(".card2:first").remove();
+      }
+      $(".card").remove();
+      //   // log the parsed data to the console
+      for (i = 0; i < data.list.length; i += 8) {
+        $("#fiveday").append(
+          "<div class = 'card mb-3 col-2'>" +
+            "<h4>" +
+            $("#city").val() +
+            "<h4/>" +
+            "<h5>" +
+            data.list[i].dt_txt.slice(0, 10) +
+            "<h5/>" +
+            "<p>" +
+            "Temp: " +
+            data.list[i].main.temp +
+            "°F" +
+            "<p/>" +
+            "<p>" +
+            "humidity: " +
+            data.list[i].main.humidity +
+            "%" +
+            "<p/>" +
+            "<p>" +
+            "Wind Speed: " +
+            data.list[i].wind.speed +
+            "mph" +
+            "<p/>" +
+            "</div>"
+        );
+        console.log($("#city").val());
+        console.log(data.list);
+        console.log(data.list[i].main.temp);
+        console.log(data.list[i].main.humidity);
+        console.log(data.list[i].wind.speed);
+        console.log(data.list[i].weather[0].icon);
+      }
     });
 }
-
-// call the getWeather function with the city name you want to get the weather for
-getWeather("New York");
